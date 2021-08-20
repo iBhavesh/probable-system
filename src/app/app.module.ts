3352722a173aff5b10dff7365651c302b09fdd2f
Bhavesh Sharma <34792643+iBhavesh@users.noreply.github.com>
2021-08-20 15:33:04 +0000

    add shopping list component,detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingDetailComponent } from './shopping-list/shopping-detail/shopping-detail.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +17,9 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
     RecipesComponent,
     RecipeItemComponent,
     RecipeListComponent,
+    RecipeDetailComponent,
+    ShoppingListComponent,
+    ShoppingDetailComponent,
   ],
   imports: [BrowserModule],
   providers: [],
